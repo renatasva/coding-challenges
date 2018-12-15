c9e8516a144427c25967950e146a1958268fc93c
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2018-12-15 23:57:17 +0000

    completed

--- ImageSlider/javascript.js
+++ ImageSlider/javascript.js
@@ -1,8 +1,10 @@
 let slides = $('.slide');
 
+let flag = 0;
+
 slides.first().before(slides.last());
 
-setInterval(show,4000);
+setInterval(show,3000);
 
 function show(){
   slides = $('.slide');
@@ -11,4 +13,13 @@ function show(){
   slides.last().after(slides.first());
 
   activeSlide.removeClass('active').next('.slide').addClass('active');
+
+  if (flag == 0){
+    $(".box").css({'-webkit-clip-path':"polygon(0% 100%, 100% 100%, 100% 90%, 85% 95%, 10% 78%, 7% 11%, 90% 5%, 85% 95%, 100% 95%, 100% 0%, 0% 0%, 0% 100%"});
+    flag = 1;
+  }
+  else{
+    $(".box").css({'-webkit-clip-path':"polygon(0% 100%, 100% 100%, 100% 80%, 93% 85%, 8% 95%, 15% 6%, 89% 22%, 93% 85%, 100% 80%, 100% 0%, 0% 0%, 0% 100%"});
+    flag = 0;
+  }
 }

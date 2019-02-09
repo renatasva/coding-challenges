7186390fa734cf8bdd1d21a8fb3942b89e72aba7
Renata Svaboviciute <38297040+renatasva@users.noreply.github.com>
2019-02-09 23:53:52 +0000

    added more functions, able to move puzzle, check collisions and rotate

--- TetrisGame/tetris.js
+++ TetrisGame/tetris.js
@@ -3,14 +3,50 @@ const context = canvas.getContext('2d');
 
 context.scale(28, 28);
 
-context.fillStyle = '#3B1B2B';
-context.fillRect(0, 0, canvas.width, canvas.height);
-
 const matrix = [
   [0, 0, 0],
   [1, 1, 0],
   [0, 1, 1],
 ];
+
+//function detecting a collision
+function collide(arena, player) {
+  //breaking out player matrix and position
+  const [m, o] = [player.matrix, player.pos];
+  //we iterate over the player
+  for (let y = 0; y < m.length; ++y) {
+    for (let x = 0; x < m[y].length; ++x) {
+      if (m[y][x] !== 0 &&
+        //we check if arena row exists (o is offset)
+          (arena[y + o.y] &&
+            //and check if column exists
+           arena[y + o.y][x + o.x]) !== 0) {
+        //returning true because collision happened
+           return true;
+      }
+    }
+  }
+  return false;
+}
+
+function createMatrix(w, h) {
+  const matrix = [];
+  //while height is not zero we decrease it by one
+  while (h--) {
+    //we push a new width array filled with zeros (empty line)
+    matrix.push(new Array(w).fill(0));
+  }
+  return matrix;
+}
+
+//draw function
+function draw() {
+  context.fillStyle = '#3B1B2B';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  drawMatrix(arena, {x: 0, y: 0});""
+  drawMatrix(player.matrix, player.pos);
+}
 
 function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
@@ -25,4 +61,135 @@ function drawMatrix(matrix, offset) {
   });
 }
 
-drawMatrix(matrix, {x: 5, y: 5});
+//function to copy the values of the player and merge it into arena at correct positions
+function merge(arena, player) {
+  player.matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        arena[y + player.pos.y][x + player.pos.x] = value;
+      }
+    });
+  });
+}
+
+function playerDrop() {
+  player.pos.y++;
+  if (collide(arena, player)) {
+    //if collision occured - set player back to top
+    player.pos.y--;
+    merge(arena, player);
+    player.pos.y = 0;
+  }
+  dropCounter = 0;
+}
+
+//moves the puzzle nad checks if there is a collision( doesn't let to go over other puzzle or outside game canvas)
+function playerMove(dir) {
+  player.pos.x += dir;
+  if (collide(arena, player)) {
+    player.pos.x -= dir;
+  }
+}
+
+function playerRotate (dir) {
+  const pos = player.pos.x;
+  let offset = 1;
+  rotate(player.matrix, dir);
+  while(collide(arena, player)) {
+    //if we collide we move the player by offset(1)
+    player.pos.x += offset;
+    //if we still collide we need to have another offset
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    //check if offset is more than player matrix first rows length
+    //means that we moved too much and we need to rotate back
+    if (offset > player.matrix[0].length) {
+      rotate(player.matrix, -dir);
+      player.pos.x = pos;
+      return;
+    }
+  }
+};
+
+function rotate(matrix, dir) {
+  for (let y = 0; y < matrix.length; ++y) {
+    for (let x = 0; x < y; ++x) {
+      [
+            matrix[x][y],
+            matrix[y][x],
+      ] = [
+            matrix[y][x],
+            matrix[x][y],
+      ];
+    }
+  }
+  //we check direction, if positive - reverse the row, if no - reverse the matrix
+  if (dir > 0) {
+    matrix.forEach(row => row.reverse());
+  } else {
+    matrix.reverse();
+  }
+}
+
+let dropCounter = 0;
+//drops every 1 second
+let dropInterval = 1000;
+
+let lastTime = 0;
+//update function to draw continuously
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if (dropCounter > dropInterval) {
+    playerDrop();
+  }
+
+  draw();
+  requestAnimationFrame(update);
+}
+
+//arena - the whole canvas area where we set width  with 12 units and hight with 20 units
+const arena = createMatrix(12, 20);
+
+//player structure that has a position and matrix
+const player = {
+  pos: {x: 2, y: 2},
+  matrix: matrix,
+};
+
+document.addEventListener('keydown', event => {
+  if (event.keyCode === 37) {
+    //goes left if press left arrow
+    playerMove(-1);
+  } else if (event.keyCode === 39) {
+    playerMove(1);
+  } else if (event.keyCode === 40) {
+    playerDrop();
+  } else if (event.keyCode === 81) {
+    playerRotate(-1);
+  } else if (event.keyCode === 87) {
+    playerRotate(1);
+  }
+})
+
+update();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

9842046fde8b5ade9044be8fcf3f092b91bf8a92
Renata Svaboviciute <38297040+renatasva@users.noreply.github.com>
2019-02-10 14:43:06 +0000

    added more puzzle shapes, colors and function to clear up after reaching the top

--- TetrisGame/tetris.js
+++ TetrisGame/tetris.js
@@ -3,11 +3,11 @@ const context = canvas.getContext('2d');
 
 context.scale(28, 28);
 
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 0],
-  [0, 1, 1],
-];
+// const matrix = [
+//   [0, 0, 0],
+//   [1, 1, 0],
+//   [0, 1, 1],
+// ];
 
 //function detecting a collision
 function collide(arena, player) {
@@ -39,6 +39,52 @@ function createMatrix(w, h) {
   return matrix;
 }
 
+function createPiece(type) {
+  if (type === 'Z') {
+    return [
+        [0, 0, 0],
+        [1, 1, 0],
+        [0, 1, 1],
+    ];
+  } else if (type === 'O') {
+    return [
+        [2, 2],
+        [2, 2],
+    ];
+  } else if (type === 'I') {
+    return [
+        [0, 3, 0, 0],
+        [0, 3, 0, 0],
+        [0, 3, 0, 0],
+        [0, 3, 0, 0],
+    ];
+  } else if (type === 'S') {
+    return [
+        [0, 0, 0],
+        [0, 4, 4],
+        [4, 4, 0],
+    ];
+  } else if (type === 'L') {
+    return [
+        [0, 5, 0],
+        [0, 5, 0],
+        [0, 5, 5],
+    ];
+  } else if (type === 'J') {
+    return [
+        [0, 6, 0],
+        [0, 6, 0],
+        [6, 6, 0],
+    ];
+  } else if (type === 'T') {
+    return [
+        [0, 0, 0],
+        [7, 7, 7],
+        [0, 7, 0],
+    ];
+  }
+}
+
 //draw function
 function draw() {
   context.fillStyle = '#3B1B2B';
@@ -52,7 +98,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = 'purple';
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x,
                          y + offset.y,
                          1, 1);
@@ -78,7 +124,7 @@ function playerDrop() {
     //if collision occured - set player back to top
     player.pos.y--;
     merge(arena, player);
-    player.pos.y = 0;
+    playerReset();
   }
   dropCounter = 0;
 }
@@ -88,6 +134,21 @@ function playerMove(dir) {
   player.pos.x += dir;
   if (collide(arena, player)) {
     player.pos.x -= dir;
+  }
+}
+
+function playerReset() {
+  const pieces = 'ZOISLJT';
+  player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+  //we place player at the top
+  player.pos.y = 0;
+  //and in the middle
+  player.pos.x = (arena[0].length / 2 | 0) -
+                 (player.matrix[0].length / 2 | 0);
+  //to check if game is over(puzzles reached the top)
+  if (collide(arena, player)) {
+    //clear the arena to start again
+    arena.forEach(row => row.fill(0));
   }
 }
 
@@ -149,13 +210,24 @@ function update(time = 0) {
   requestAnimationFrame(update);
 }
 
+const colors = [
+  null,
+  'purple',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'aqua'
+];
+
 //arena - the whole canvas area where we set width  with 12 units and hight with 20 units
 const arena = createMatrix(12, 20);
 
 //player structure that has a position and matrix
 const player = {
-  pos: {x: 2, y: 2},
-  matrix: matrix,
+  pos: {x: 5, y: 0},
+  matrix: createPiece('O'),
 };
 
 document.addEventListener('keydown', event => {
